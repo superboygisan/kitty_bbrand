09a456a3ee272ebfd3609bc8e8f4a38c04ed1698
superboygisan <superboygisan@gmail.com>
2026-06-09 23:56:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -42,7 +42,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/GANJAKHOR001/FINAL_BRAND",
+    "https://github.com/superboygisan/kitty_bbrand",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
